8eed369f13b9f8c4e53de447d0a8a4a7b9265c2e
DHAIRYA GUPTA <122604403+guptadhairy@users.noreply.github.com>
2024-07-09 16:41:42 +0530

    D-9-7-24

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -7,7 +7,7 @@ const QuestionBox = ({ question }) => {
 
   return (
     <div className="p-4 bg-white shadow-md rounded-lg sm:w-[900px] w-full">
-      <div className="text-xl font-semibold mb-2">{question.title}</div>
+      <div className="text-xl font-semibold mb-2 bg-gradient-to-br from-[#787ef1] to-[#3710e7] bg-clip-text text-transparent">{question.title}</div>
       <div className="mb-4">{question.description}</div>
       <JoditEditor
         ref={editor}
@@ -16,7 +16,7 @@ const QuestionBox = ({ question }) => {
         onBlur={(newContent) => setAnswer(newContent)} // preferred to use only this option to update the content for performance reasons
       />
 
-      <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg">
+      <button className="mt-4 bg-gradient-to-br from-[#787ef1] to-[#3710e7]  text-white py-2 px-4 rounded-xl">
         Submit
       </button>
     </div>
